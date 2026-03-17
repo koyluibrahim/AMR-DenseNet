1c9723de0cf918582547e7c0d7fd9a13afa3f222
İbrahim Köylü <119944742+koyluibrahim@users.noreply.github.com>
2026-03-17 13:29:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,128 +1,112 @@
-﻿import os,random
-os.environ["KERAS_BACKEND"] = "tensorflow"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import os
+import random
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt 
-from matplotlib.collections import LineCollection
-from matplotlib.colors import ListedColormap, BoundaryNorm
-import pickle, random, sys,h5py
-import keras
-import keras.backend as K
-from keras.callbacks import LearningRateScheduler
-from keras.regularizers import *
-from keras.optimizers import adam
-from keras.models import model_from_json
+import matplotlib.pyplot as plt
+import pickle
 import csv
-import mltools,dataset2016
-import rmlmodels.DenseNet as mcl
+import sys
+
+# Keras 3 ve TF 2.x için ortam ayarları
+os.environ["KERAS_BACKEND"] = "tensorflow"
 import tensorflow as tf
-import keras.backend.tensorflow_backend as KTF
+from tensorflow import keras
+from tensorflow.keras import layers, models, optimizers
 
-(mods,snrs,lbl),(X_train,Y_train),(X_val,Y_val),(X_test,Y_test),(train_idx,val_idx,test_idx) = \
+# Modül bağımlılıklarını Colab'e tanıtmak için
+import mltools, dataset2016
+import rmlmodels.DenseNet as mcl
+
+# Veriyi yükle
+(mods, snrs, lbl), (X_train, Y_train), (X_val, Y_val), (X_test, Y_test), (train_idx, val_idx, test_idx) = \
     dataset2016.load_data()
-X_train=np.expand_dims(X_train,axis=3)
-X_test=np.expand_dims(X_test,axis=3)
-X_val=np.expand_dims(X_val,axis=3)
 
-print(X_train.shape)
+# Boyutları genişlet (Keras 3 için gerekli)
+X_train = np.expand_dims(X_train, axis=3).astype('float32')
+X_test = np.expand_dims(X_test, axis=3).astype('float32')
+X_val = np.expand_dims(X_val, axis=3).astype('float32')
+
+print(f"Eğitim verisi şekli: {X_train.shape}")
 classes = mods
 
+# Klasörleri oluştur (Hata almamak için)
+os.makedirs('weights', exist_ok=True)
+os.makedirs('figure', exist_ok=True)
+os.makedirs('predictresult', exist_ok=True)
 
-# Set up some params
-nb_epoch = 10000     # number of epochs to train on
-batch_size = 400 # training batch size
+# Model Parametreleri
+nb_epoch = 100 
+batch_size = 400 
 
-model=mcl.DenseNet()
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+# Model Kurulumu
+model = mcl.DenseNet()
+
+# KRİTİK DEĞİŞİKLİK: 'adam' yerine güncel Optimizer nesnesi ve learning_rate
+model.compile(
+    loss='categorical_crossentropy', 
+    metrics=['accuracy'], 
+    optimizer=optimizers.Adam(learning_rate=0.001)
+)
+
 model.summary()
 
 filepath = 'weights/weights.h5'
-history = model.fit(X_train,
-    Y_train,
+
+# Eğitim
+history = model.fit(
+    X_train, Y_train,
     batch_size=batch_size,
     epochs=nb_epoch,
     verbose=2,
-    validation_data=(X_val,Y_val),
-    callbacks = [
-                keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto'),
-                keras.callbacks.ReduceLROnPlateau(monitor='val_loss',factor=0.5,verbose=1,patince=5,min_lr=0.0000001),
-                keras.callbacks.EarlyStopping(monitor='val_loss', patience=50, verbose=1, mode='auto')
+    validation_data=(X_val, Y_val),
+    callbacks=[
+        keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto'),
+        # 'patince' yazım hatası 'patience' olarak düzeltildi
+        keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, verbose=1, patience=5, min_lr=0.0000001),
+        keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, verbose=1, mode='auto')
+    ]
+)
 
-                ]
-                    )
+# Sonuçları Göster
 mltools.show_history(history)
-# #
-# #Show simple version of performance
-score = model.evaluate(X_test, Y_test, verbose=1, batch_size=batch_size)
-print(score)
 
-def predict(model):
+# Tahmin ve Görselleştirme Fonksiyonu (Predict)
+def predict_and_plot(model):
     model.load_weights(filepath)
-    # Plot confusion matrix
     test_Y_hat = model.predict(X_test, batch_size=batch_size)
-    confnorm,_,_ = mltools.calculate_confusion_matrix(Y_test,test_Y_hat,classes)
-    mltools.plot_confusion_matrix(confnorm, labels=['8PSK','AM-DSB','AM-SSB','BPSK','CPFSK','GFSK','4-PAM','16-QAM','64-QAM','QPSK','WBFM'],save_filename='figure/sclstm-a_total_confusion')
+    
+    # mltools içindeki fonksiyonun parametre sırasına göre kontrol et
+    confnorm, _, _ = mltools.calculate_confusion_matrix(Y_test, test_Y_hat, classes)
+    
+    # Toplam Karmaşıklık Matrisi
+    mltools.plot_confusion_matrix(confnorm, labels=classes, save_filename='figure/total_confusion')
 
-    # Plot confusion matrix
     acc = {}
-    acc_mod_snr = np.zeros( (len(classes),len(snrs)) )
-    i = 0
-    for snr in snrs:
+    acc_mod_snr = np.zeros((len(classes), len(snrs)))
+    
+    for i, snr in enumerate(snrs):
+        # SNR bazlı filtreleme
         test_SNRs = [lbl[x][1] for x in test_idx]
-        test_X_i=X_test[np.where(np.array(test_SNRs) == snr)]
-        test_Y_i = Y_test[np.where(np.array(test_SNRs) == snr)]
+        idx_i = np.where(np.array(test_SNRs) == snr)
+        test_X_i = X_test[idx_i]
+        test_Y_i = Y_test[idx_i]
 
-        # estimate classes
         test_Y_i_hat = model.predict(test_X_i)
-        confnorm_i,cor,ncor = mltools.calculate_confusion_matrix(test_Y_i,test_Y_i_hat,classes)
+        confnorm_i, cor, ncor = mltools.calculate_confusion_matrix(test_Y_i, test_Y_i_hat, classes)
+        
         acc[snr] = 1.0 * cor / (cor + ncor)
-        result = cor / (cor + ncor)
-        with open('acc111.csv', 'a', newline='') as f0:
-            write0 = csv.writer(f0)
-            write0.writerow([result])
-        mltools.plot_confusion_matrix(confnorm_i, labels=['8PSK','AM-DSB','AM-SSB','BPSK','CPFSK','GFSK','4-PAM','16-QAM','64-QAM','QPSK','WBFM'], title="Confusion Matrix",save_filename="figure/Confusion(SNR=%d)(ACC=%2f).png" % (snr,100.0*acc[snr]))
+        acc_mod_snr[:, i] = np.round(np.diag(confnorm_i)/np.sum(confnorm_i, axis=1), 3)
+        
+        print(f"SNR {snr}dB için Doğruluk: {acc[snr]:.4f}")
 
-        acc_mod_snr[:,i] = np.round(np.diag(confnorm_i)/np.sum(confnorm_i,axis=1),3)
-        i = i +1
+    # SNR vs Accuracy Grafiği
+    plt.figure(figsize=(10, 6))
+    plt.plot(snrs, [acc[x] for x in snrs], marker='o')
+    plt.xlabel("SNR (dB)")
+    plt.ylabel("Doğruluk")
+    plt.title("SNR Bazlı Sınıflandırma Başarısı")
+    plt.grid(True)
+    plt.savefig('figure/overall_acc_vs_snr.png')
+    plt.show()
 
-    #plot acc of each mod in one picture
-    dis_num=11
-    for g in range(int(np.ceil(acc_mod_snr.shape[0]/dis_num))):
-        assert (0 <= dis_num <= acc_mod_snr.shape[0])
-        beg_index = g*dis_num
-        end_index = np.min([(g+1)*dis_num,acc_mod_snr.shape[0]])
-
-        plt.figure(figsize=(12, 10))
-        plt.xlabel("Signal to Noise Ratio")
-        plt.ylabel("Classification Accuracy")
-        plt.title("Classification Accuracy for Each Mod")
-
-        for i in range(beg_index,end_index):
-            plt.plot(snrs, acc_mod_snr[i], label=classes[i])
-            # 设置数字标签
-            for x, y in zip(snrs, acc_mod_snr[i]):
-                plt.text(x, y, y, ha='center', va='bottom', fontsize=8)
-
-        plt.legend()
-        plt.grid()
-        plt.savefig('figure/acc_with_mod_{}.png'.format(g+1))
-        plt.close()
-    #save acc for mod per SNR
-    fd = open('predictresult/acc_for_mod.dat', 'wb')
-    pickle.dump((acc_mod_snr), fd)
-    fd.close()
-
-    # Save results to a pickle file for plotting later
-    print(acc)
-    fd = open('predictresult/acc.dat','wb')
-    pickle.dump( (acc) , fd )
-
-    # Plot accuracy curve
-    plt.plot(snrs, list(map(lambda x: acc[x], snrs)))
-    plt.xlabel("Signal to Noise Ratio")
-    plt.ylabel("Classification Accuracy")
-    plt.title(" Classification Accuracy on RadioML 2016.10 Alpha")
-    plt.tight_layout()
-    plt.savefig('figure/each_acc.png')
-predict(model)
+# Fonksiyonu çalıştır
+predict_and_plot(model)
